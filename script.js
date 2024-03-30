53e3abd0e48cbc56bf952f7488e3b8af7f5d18b9
Gulay <gulaymovlamova@gmail.com>
2024-03-30 15:54:38 +0400

    feat: price

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const seatContainer = document.querySelector('#seatContainer')
 const filmContainer = document.querySelector('#filmContainer')
 const finalInfo = document.querySelector('#finalInfo')
+let sum = 0
 
 const films = [
      {
@@ -27,29 +28,32 @@ const films = [
 
 
 
-const calculate = () => {
+const calculate = (sum) => {
     let html = ''
     for(let film of films) {
-        html = `<div class="text-white mb-[15px]">Siz ${film.name} filmine 3 bilet aldiniz, 27 manat odemelisiniz.</div>`
+        if(film.element.classList.contains('this')) 
+        html += `<div class="text-white mb-[15px]">Siz ${film.name} filmine ${sum} bilet aldiniz, ${sum*film.price} manat odemelisiniz.</div>`
     }
     finalInfo.innerHTML = html
-    
 }
 
 
 
 seatContainer.addEventListener('click', e => {
+    
     if (e.target.matches('button')) {
         if (e.target.classList.contains('bg-[#5B5B5B]')) {
             e.target.classList.remove('bg-[#5B5B5B]')
             e.target.classList.add('bg-[#FF9446]')
+            sum++
         }
         else if (e.target.classList.contains('bg-[#FF9446]')) {
             e.target.classList.remove('bg-[#FF9446]')
             e.target.classList.add('bg-[#5B5B5B]')
+            sum--
         }
     }
-
+calculate(sum)
 })
 
 
@@ -60,10 +64,15 @@ filmContainer.addEventListener('click', e => {
             if (film.element.classList.contains('bg-[#66B979]')) {
                 film.element.classList.remove('bg-[#66B979]')
                 film.element.classList.add('bg-[#5B5B5B]')
+                film.element.classList.remove('this')
             }
         }
         e.target.classList.remove('bg-[#5B5B5B]')
         e.target.classList.add('bg-[#66B979]')
+        e.target.classList.add('this')
+    }
+    else if (e.target.matches('button') && e.target.classList.contains('bg-[#66B979]')) {
+        e.target.classList.add('this')
     }
 })
 
